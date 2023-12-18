3183e8015c2fedc7f0b8ded34252a10009c7bd2e
Andrew Sanchez <druu831@gmail.com>
2023-12-18 13:26:11 -0800

    Feat. added projects section

--- src/components/section-heading.tsx
+++ src/components/section-heading.tsx
@@ -7,5 +7,9 @@ interface SectionHeadingProps {
 export default function SectionHeading(props: SectionHeadingProps) {
   const { children } = props;
 
-  return <h2 className='text-3xl font-medium capitalize mb-8'>{children}</h2>;
+  return (
+    <h2 className='text-3xl font-medium capitalize mb-8 text-center'>
+      {children}
+    </h2>
+  );
 }
