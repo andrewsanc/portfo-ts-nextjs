55fb83cfe53fe60fde00827fa8a5a8dbc15d3153
Andrew Sanchez <druu831@gmail.com>
2023-12-18 18:23:41 -0800

    Fix. corrected linting errors

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -20,7 +20,7 @@ export default function About() {
       <p className='mb-3'>
         After graduating with a degree in{" "}
         <span className='font-medium'>Computer Science</span> I used my passion
-        for programming to solve problems and improve people's lives.{" "}
+        for programming to solve problems and improve people&apos;s lives.{" "}
         <span className='italic'>My favorite part of programming</span> is the
         problem-solving aspect. I <span className='underline'>love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
@@ -28,16 +28,16 @@ export default function About() {
         <span className='font-medium'>
           React, Next.js, Node.js, and MongoDB
         </span>
-        . I'm also familiar with TypeScript and Prisma. I'm always looking to
-        learn new technologies. I'm currently looking for a{" "}
+        . I&apos;m also familiar with TypeScript and Prisma. I&apos;m always
+        looking to learn new technologies. I&apos;m currently looking for a{" "}
         <span className='font-medium'>full-time position</span> as a{" "}
         <span className='font-medium'>Software Engineer</span>.
       </p>
 
       <p>
-        <span className='italic'>When I'm not coding</span>, I enjoy working
-        out, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className='font-medium'>learning try new things</span>. I'm
+        <span className='italic'>When I&apos;m not coding</span>, I enjoy
+        working out, watching movies, and playing with my dog. I also enjoy{" "}
+        <span className='font-medium'>learning try new things</span>. I&apos;m
         currently learning about{" "}
         <span className='font-medium'>cooking and philosophy</span>.
       </p>
--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -52,7 +52,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className='font-bold'>Hello, I'm Andrew.</span> I'm a{" "}
+        <span className='font-bold'>Hello, I&apos;m Andrew.</span> I&apos;m a{" "}
         <span className='font-bold'>Full-Stack Engineer</span> with{" "}
         <span className='font-bold'>4 years</span> of experience. I enjoy
         building <span className='italic'>sites & apps</span>. My focus is{" "}
