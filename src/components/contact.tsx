9d3b69de3e9fb1f223815c6a5f9f952c8d84614b
Andrew Sanchez <druu831@gmail.com>
2023-12-19 15:18:52 -0800

    Feat. added form component

---
+++ src/components/contact.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { motion } from "framer-motion";
+import SectionHeading from "./section-heading";
+import { FaPaperPlane } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function Contact() {
+  const { ref } = useSectionInView({ sectionName: "Contact" });
+
+  return (
+    <motion.section
+      id='contact'
+      className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center'
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
+    >
+      <SectionHeading>Contact me</SectionHeading>
+      <p className='text-gray-700 -mt-6'>
+        Please contact me directly at{" "}
+        <a href='mailto:druu831@gmail.com' className='underline'>
+          druu831@gmail.com
+        </a>{" "}
+        or through this form.
+      </p>
+
+      <form className='mt-10 flex flex-col'>
+        <input
+          type='email'
+          className='h-14 rounded-lg borderBlack px-4'
+          placeholder='Your email'
+        />
+        <textarea
+          className='h-52 my-3 rounded-lg borderBlack p-4'
+          placeholder='Your message'
+        />
+        <button
+          type='submit'
+          className='group h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none flex items-center justify-center gap-2 transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950'
+        >
+          Submit
+          <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1' />
+        </button>
+      </form>
+    </motion.section>
+  );
+}
--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -9,43 +9,51 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
+import { useInView } from "react-intersection-observer";
 
 export default function Experience() {
   const { ref } = useSectionInView({ sectionName: "Experience" });
 
   return (
-    <section id='experience' ref={ref} className='scroll-mt-28 '>
+    <section id='experience' ref={ref} className='scroll-mt-28 mb-28 sm:mb-40'>
       <SectionHeading>My experiences</SectionHeading>
-      <VerticalTimeline animate={false} lineColor=''>
+      <VerticalTimeline lineColor=''>
         {experiencesData.map(
-          ({ title, icon, location, description, date }, index) => (
-            <React.Fragment key={index}>
-              <VerticalTimelineElement
-                icon={icon}
-                iconStyle={{
-                  background: "white",
-                  fontSize: "1.5rem",
-                }}
-                contentStyle={{
-                  background: "#f3f4f6",
-                  boxShadow: "none",
-                  border: "1px solid rgba(0,0,0,0.05)",
-                  textAlign: "left",
-                  padding: "1.3rem 2rem",
-                }}
-                contentArrowStyle={{
-                  borderRight: "0.4rem solid #9ca3af",
-                }}
-                date={date}
-              >
-                <h3 className='font-semibold capitalize'>{title}</h3>
-                <p className='font-normal !mt-0'>{location}</p>
-                <p className='!font-normal !mt-1 text-gray-700'>
-                  {description}
-                </p>
-              </VerticalTimelineElement>
-            </React.Fragment>
-          )
+          ({ title, icon, location, description, date }, index) => {
+            const { ref, inView } = useInView({
+              triggerOnce: true,
+            });
+
+            return (
+              <div key={index} ref={ref} className='vertical-timeline-element'>
+                <VerticalTimelineElement
+                  icon={icon}
+                  iconStyle={{
+                    background: "white",
+                    fontSize: "1.5rem",
+                  }}
+                  contentStyle={{
+                    background: "#f3f4f6",
+                    boxShadow: "none",
+                    border: "1px solid rgba(0,0,0,0.05)",
+                    textAlign: "left",
+                    padding: "1.3rem 2rem",
+                  }}
+                  contentArrowStyle={{
+                    borderRight: "0.4rem solid #9ca3af",
+                  }}
+                  visible={inView}
+                  date={date}
+                >
+                  <h3 className='font-semibold capitalize'>{title}</h3>
+                  <p className='font-normal !mt-0'>{location}</p>
+                  <p className='!font-normal !mt-1 text-gray-700'>
+                    {description}
+                  </p>
+                </VerticalTimelineElement>
+              </div>
+            );
+          }
         )}
       </VerticalTimeline>
     </section>
