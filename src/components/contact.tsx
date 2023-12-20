21ddec13ec125187a2a296acbeea396c0f435f22
Andrew Sanchez <druu831@gmail.com>
2023-12-20 14:05:50 -0800

    Fix. fix clientside error

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -40,12 +40,7 @@ export default function Contact() {
       <form
         className='mt-10 flex flex-col dark:text-black/70'
         action={async (formData) => {
-          const { error } = await sendEmail(formData);
-
-          if (error) {
-            toast.error(error);
-            return;
-          }
+          await sendEmail(formData);
 
           toast.success("Your message was sent successfully!");
         }}
