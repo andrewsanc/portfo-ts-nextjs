e859fbb1a15e2b520d057f8fc8775c6ea0a6883e
Andrew Sanchez <druu831@gmail.com>
2023-12-19 17:40:25 -0800

    Fix. fixed linting error with timeline-experiences

---
+++ src/components/experiences.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import { useSectionInView } from "@/lib/hooks";
+import SectionHeading from "./section-heading";
+import { VerticalTimeline } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
+import TimelineExperience from "./timeline-experience";
+
+export default function Experience() {
+  const { ref } = useSectionInView({ sectionName: "Experience" });
+
+  return (
+    <section id='experience' ref={ref} className='scroll-mt-28 mb-28 sm:mb-40'>
+      <SectionHeading>My experiences</SectionHeading>
+      <VerticalTimeline lineColor=''>
+        {experiencesData.map((experience, index) => (
+          <React.Fragment key={index}>
+            <TimelineExperience index={index} experience={experience} />
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
+    </section>
+  );
+}
---
+++ src/components/timeline-experience.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Experience } from "@/lib/types";
+import { useInView } from "react-intersection-observer";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+
+interface TimelineExperienceProps {
+  index: number;
+  experience: Experience;
+}
+
+export default function TimelineExperience(props: TimelineExperienceProps) {
+  const {
+    experience: { icon, date, title, description, location },
+    index,
+  } = props;
+
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
+
+  return (
+    <div key={index} ref={ref} className='vertical-timeline-element'>
+      <VerticalTimelineElement
+        icon={icon}
+        iconStyle={{
+          background: "white",
+          fontSize: "1.5rem",
+        }}
+        contentStyle={{
+          background: "#f3f4f6",
+          boxShadow: "none",
+          border: "1px solid rgba(0,0,0,0.05)",
+          textAlign: "left",
+          padding: "1.3rem 2rem",
+        }}
+        contentArrowStyle={{
+          borderRight: "0.4rem solid #9ca3af",
+        }}
+        visible={inView}
+        date={date}
+      >
+        <h3 className='font-semibold capitalize'>{title}</h3>
+        <p className='font-normal !mt-0'>{location}</p>
+        <p className='!font-normal !mt-1 text-gray-700'>{description}</p>
+      </VerticalTimelineElement>
+    </div>
+  );
+}
