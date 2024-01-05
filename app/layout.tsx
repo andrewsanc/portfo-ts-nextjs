cb4ce6d9f7cce12fc31a5b61de86d5ff975291a7
Andrew Sanchez <druu831@gmail.com>
2024-01-05 09:02:56 -0800

    feat. added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,6 +38,7 @@ export default function RootLayout({
             <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
+        <Analytics />
       </body>
     </html>
   );
