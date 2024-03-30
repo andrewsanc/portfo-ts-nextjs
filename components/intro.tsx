2097710d666fe9c4f95da7559cee8466177b094d
TB831 <druu831@gmail.com>
2024-03-30 10:59:00 -0700

    feat. updated skills and resume download link

--- components/intro.tsx
+++ components/intro.tsx
@@ -13,26 +13,26 @@ export default function Intro() {
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
-    <section ref={ref} className='mb-28 max-w-[50rem] text-center sm:mb-0'>
-      <div className='flex items-center justify-center'>
-        <div className='relative'>
+    <section ref={ref} className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ type: "tween", duration: "0.2" }}
           >
             <Image
-              src='/portrait.png'
-              width='192'
-              height='192'
-              quality='95'
-              alt='Andrew Sanchez portrait'
-              className='h-36 w-36 rounded-full border-[0.35rem] border-white object-cover shadow-xl'
+              src="/portrait.png"
+              width="192"
+              height="192"
+              quality="95"
+              alt="Andrew Sanchez portrait"
+              className="h-36 w-36 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
               priority
             />
           </motion.div>
           <motion.span
-            className='absolute bottom-0 right-0 text-4xl'
+            className="absolute bottom-0 right-0 text-4xl"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
@@ -48,53 +48,53 @@ export default function Intro() {
       </div>
 
       <motion.h1
-        className='mv-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+        className="mv-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className='font-bold'>Hello, I&apos;m Andrew.</span> I&apos;m a{" "}
-        <span className='font-bold'>Full-Stack Engineer</span> with{" "}
-        <span className='font-bold'>4 years</span> of experience. I enjoy
-        building <span className='italic'>sites & apps</span>. My focus is{" "}
-        <span className='underline'>
+        <span className="font-bold">Hello, I&apos;m Andrew.</span> I&apos;m a{" "}
+        <span className="font-bold">Full-Stack Engineer</span> with{" "}
+        <span className="font-bold">4 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">
           React (Next.js), Nodejs, Expressjs, Prisma, PostgreSQL,
         </span>{" "}
-        and <span className='underline'>MongoDB</span>.
+        and <span className="underline">MongoDB</span>.
       </motion.h1>
 
       <motion.div
-        className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium py-4'
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium py-4"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.1 }}
       >
         <Link
-          href='#contact'
-          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
           }}
         >
           Contact me here{" "}
-          <BsArrowRight className='opacity-70 group-hover:translate-x-2 transition' />
+          <BsArrowRight className="opacity-70 group-hover:translate-x-2 transition" />
         </Link>
         <a
-          className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition borderBlack dark:bg-white/10 dark:text-white/60'
-          href='https://drive.google.com/uc?export=download&id=1wPzdWP6sb_ffa2FIhId5F9AxId3NBZ2O'
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://drive.google.com/uc?export=download&id=1MAo7D13qv_RaKtYBmsj7Ppxb4ON4YD6G"
         >
           Download CV{" "}
-          <HiDownload className='opacity-60 group-hover:translate-y-2 transition' />
+          <HiDownload className="opacity-60 group-hover:translate-y-2 transition" />
         </a>
         <a
-          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition borderBlack dark:bg-white/10 dark:text-white/60'
-          href='https://www.linkedin.com/in/andrewsanc/'
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://www.linkedin.com/in/andrewsanc/"
         >
           <BsLinkedin />
         </a>
         <a
-          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition borderBlack dark:bg-white/10 dark:text-white/60'
-          href='https://github.com/andrewsanc'
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/andrewsanc"
         >
           <FaGithubSquare />
         </a>
